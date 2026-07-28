ef401d5f148cbfabf30fc3f7a31643eb684b1afb
Gem2910 <100980568+Gem2910@users.noreply.github.com>
2026-07-28 19:30:03 +0700

    Route backend path to redesigned app layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import BackendDashboard from "./components/BackendDashboard";
 import "./index.css";
-
-const pathname = window.location.pathname.replace(/\/+$/, "") || "/";
-const RootView = pathname === "/backend" ? BackendDashboard : App;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RootView />
+    <App />
   </StrictMode>
 );
